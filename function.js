cb7d7cf6a3a9cd6ef3187ce55d7db799758dde09
Ghanshyam Prajapati <89632088+ghanshyam-prajapati-it@users.noreply.github.com>
2021-12-15 09:01:04 +0530

    Add files via upload

---
+++ function.js
@@ -0,0 +1,49 @@
+function asdf (loot1, loot2, loot3)
+    {
+        var l = loot1 + loot2 + loot3;
+        document.write(l);
+        return(l);
+    }
+    asdf(14,5,9)
+    
+
+    /* function minus (val1, val2, val3)
+    {
+        var rt = (val1 - val2 - val3);
+        document.write(rt + "<br>");
+        return(rt);
+    }
+    var we = minus(10,3,2);
+    var pp = we * 10;
+    document.write("<b>" + pp); */
+
+    /* function abc (arg1, arg2)
+    {
+        var z = arg1 + arg2;
+        document.write(z);
+        return(z);
+    }
+    abc(13,8); */
+
+
+        /* function add (a1, a2)
+        {
+            var a  =  a1 * a2;
+            document.write(a)
+            return(a)
+        }
+        add (13,2) */
+
+
+
+    /* function abc (arg1, arg2, arg3)
+    {
+        var a = arg1 + arg2 + arg3;
+        document.write("Your Sum = " + a + "; Ans * 10 = ");
+        return(a);
+    }
+    var y = abc(12,1,10)
+    var z = y * 10;
+    document.write(z); */
+
+
